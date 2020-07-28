f4e71654f4a5ec1c2ab07836db04e85439a9843b
todwal g <66479707+g8todwal@users.noreply.github.com>
2020-07-28 15:22:45 +0530

    Add files via upload

---
+++ src/redux/ActionTypes.js
@@ -0,0 +1,20 @@
+// ======for disehes=========
+export const DISHES_LOADING = 'DISHES_LOADING';
+export const DISHES_FAILED = 'DISHES_FAILED';
+export const ADD_DISHES = 'ADD_DISHES';
+
+// ======for comments=========
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const COMMENTS_FAILED = 'COMMENTS_FAILED';
+export const ADD_COMMENTS = 'ADD_COMMENTS';
+
+// ======for promotions=========
+export const PROMOS_LOADING = 'PROMOS_LOADING';
+export const PROMOS_FAILED = 'PROMOS_FAILED';
+export const ADD_PROMOS = 'ADD_PROMOS';
+
+// ======for leaders=========
+export const LEADERS_LOADING = 'LEADERSS_LOADING';
+export const LEADERS_FAILED = 'LEADERS_FAILED';
+export const ADD_LEADERS = 'ADD_LEADERS';
+
